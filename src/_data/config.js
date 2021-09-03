f96416a58b9b16dd60218d7d8fe9acebc6b14b4a
Pavel Sanchez <23495830+PaleBluDot@users.noreply.github.com>
2021-09-03 18:21:06 -0400

    chore(config): 11ty file structure

---
+++ src/_data/config.js
@@ -0,0 +1,36 @@
+module.exports = {
+	environment: process.env.ELEVENTY_ENV,
+	company: "American Technion Society",
+	emailTitle: "ATS Email",
+	address: {
+		line1: "55 East 59th Street",
+		line2: "",
+		city: "New York",
+		state: "NY",
+		zip: "10022",
+	},
+	images: {
+		logo: {
+			default: "http://support.ats.org/images/content/pagebuilder/ATS-logo-blue.png",
+			white: "https://secure2.convio.net/ats/images/content/pagebuilder/ats-logo-white.png",
+			alumni: "http://support.ats.org/images/content/pagebuilder/alumni-logo.png",
+			alumniWhite: "",
+		},
+		icons: {
+			fb: "https://secure2.convio.net/ats/images/content/pagebuilder/footer-facebook.png",
+			ig: "https://secure2.convio.net/ats/images/content/pagebuilder/footer-instagram.png",
+			tw: "https://secure2.convio.net/ats/images/content/pagebuilder/footer-twitter.png",
+		}
+	},
+	links: {
+		donate: "https://ats.org",
+		privacy: "https://pavelsanchez.com/privacy",
+		unsub: "https://pavelsanchez.com/unsub",
+		url: "https://ats.org",
+	},
+	signature: {
+		name: "First and Last Name",
+		title: "title",
+		headshot: "headshot",
+	}
+};
