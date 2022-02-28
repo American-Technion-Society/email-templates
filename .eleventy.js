02fe9c23e4fadf565af71bc0899a19f1313232b7
Pavel Sanchez <23495830+PaleBluDot@users.noreply.github.com>
2022-02-27 20:09:14 -0500

    refactor: update to 11ty (#9)
    * chore: remove extra templates
    
    * chore: update dictionaries
    
    * chore: add layout alias
    
    * chore: ignore all files that have test
    
    * chore: update dependencies
    
    * chore: add btn shortcode
    
    * chore: ignore data config file
    
    * feat: update theme vars
    
    * feat: add default layout
    
    * refactor: update to nunjucks
    
    * feat: create nunjucks partials
    
    * feat: create demo file
    
    * refactor: BREAKING move to 11ty components
    
    * chore: update workflow
    
    * chore: update workflow

--- .eleventy.js
+++ .eleventy.js
@@ -9,6 +9,32 @@ module.exports = function (eleventyConfig) {
 	eleventyConfig.setLibrary("md", markdownLib);
 
 	eleventyConfig.addLayoutAlias("index", "layouts/index.njk");
+	eleventyConfig.addLayoutAlias("default", "layouts/default.njk");
+
+	// Universal Shortcodes
+	eleventyConfig.addShortcode("button", function (color, colorBg, btnText) {
+		return `
+			<!-- BUTTON SECTION -->
+			<div align="center">
+				<table width="100%" border="0" cellspacing="0" cellpadding="0" role="presentation">
+					<tr>
+						<td align="center">
+							<table border="0" cellspacing="0" cellpadding="0" role="button">
+								<tr>
+									<td align="center" bgcolor="${colorBg}">
+										<a href="{{config.links.home}}" style="font-family: Arial, sans-serif; color: ${color}; padding: 12px 18px; border: 1px solid transparent; display: inline-block;font-size: 18px;font-weight:bold;text-align:center;text-decoration:none;text-transform: uppercase;">
+											<span class="btn">${btnText}</span>
+										</a>
+									</td>
+								</tr>
+							</table>
+						</td>
+					</tr>
+				</table>
+				<br />
+			</div>
+		`;
+	});
 
 	return {
 		htmlTemplateEngine: "njk",
--- src/_data/config.js
+++ src/_data/config.js
@@ -1,36 +1,67 @@
 module.exports = {
 	environment: process.env.ELEVENTY_ENV,
-	company: "American Technion Society",
-	emailTitle: "ATS Email",
+	name: 'American Technion Society',
+	title: "ATS Email",
+	salutation: "Dear [[?xx[[S1:first_name]]xx::xxxx::Friend::[[S1:first_name]]]]",
 	address: {
-		line1: "55 East 59th Street",
-		line2: "",
-		city: "New York",
-		state: "NY",
-		zip: "10022",
+		line1: '55 East 59th Street',
+		line2: '',
+		city: 'New York',
+		state: 'NY',
+		zip: '10022',
 	},
-	images: {
-		logo: {
-			default: "http://support.ats.org/images/content/pagebuilder/ATS-logo-blue.png",
-			white: "https://secure2.convio.net/ats/images/content/pagebuilder/ats-logo-white.png",
-			alumni: "http://support.ats.org/images/content/pagebuilder/alumni-logo.png",
-			alumniWhite: "",
-		},
-		icons: {
-			fb: "https://secure2.convio.net/ats/images/content/pagebuilder/footer-facebook.png",
-			ig: "https://secure2.convio.net/ats/images/content/pagebuilder/footer-instagram.png",
-			tw: "https://secure2.convio.net/ats/images/content/pagebuilder/footer-twitter.png",
-		}
+	logo: {
+		blue: 'http://support.ats.org/images/content/pagebuilder/ATS-logo-blue.png',
+		white: 'https://secure2.convio.net/ats/images/content/pagebuilder/ats-logo-white.png',
+		alumni: 'http://support.ats.org/images/content/pagebuilder/alumni-logo.png',
+		alumniWhite: '',
 	},
 	links: {
-		donate: "https://ats.org",
-		privacy: "https://pavelsanchez.com/privacy",
-		unsub: "https://pavelsanchez.com/unsub",
-		url: "https://ats.org",
+		home: 'https://ats.org',
+		donate: 'https://secure2.convio.net/ats/site/Donation2?df_id=2302&mfc_pref=T&2302.donation=form1&s_src=email&s_subsrc=footer_donate',
+		privacy: 'https://ats.org/privacy-policy/',
+		unsub: 'http://support.ats.org/site/CO',
 	},
 	signature: {
-		name: "First and Last Name",
-		title: "title",
-		headshot: "headshot",
-	}
+		name: 'Michael Waxman-Lenz',
+		title: 'Chief Executive Officer',
+		sign: 'https://secure2.convio.net/ats/images/content/pagebuilder/Michael_Waxman-Lenz_Signature.png?t=1643840151707',
+		headshot: '',
+	},
+	theme: {
+		colors: {
+			atsBlue: '#194595',
+			lightSeaGreen: '#3ebbb1',
+			pineGreen: '#037b77',
+			goldfish: '#f7942f',
+			altPink: '#c3217f',
+			altRed: '#dd4f26',
+			altBlue: '#0090af',
+			white: '#ffffff',
+			test: "#2b365e",
+			light: {
+				text: '#cecece',
+				box: '#f3f3f3',
+				bg: '#ffffff',
+			},
+			dark: {
+				text: '#3c3532',
+				smallText: '#333333',
+				box: '#252527',
+				bg: '#1c1c1e',
+			}
+		},
+		font: {
+			size: '18px',
+			family: 'Arial, Verdana, Helvetica, sans-serif',
+			weight: 'normal',
+			lineHeight: '21px',
+			textAlign: 'left',
+			centerAlign: 'center',
+		},
+		default: {
+			table: 'role="presentation" align="center" border="0" cellpadding="0" cellspacing="0" class="emailwrapto100pc" width="600"',
+			td: 'class="emailbodytext" style="color: #3c3532; font-family: Arial, Verdana, Helvetica, sans-serif; font-size: 18px; line-height: 26px; padding: 15px 0 20px 0" valign="top"',
+		},
+	},
 };
